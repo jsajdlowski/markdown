10706d585b614dfe988fe9491fc3496f21551212
jsajdlowski <91980509+jsajdlowski@users.noreply.github.com>
2022-01-30 04:00:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-def factorial(n):
+import os 
+import math
+
+
+def factorial(n) -> int:
     if n >= 1:
         fact = 1
         for num in range(2, n + 1):
             fact *= num
         return fact
 
-def is_lower(str):
-    flag = False
-    if str == str.lower():
-        flag = True
-    else:
-        flag = False
-    return flag
-
-print(is_lower(0))
+    
+def subtraction(x,y) -> int:
+    return math.floor(x-y)
